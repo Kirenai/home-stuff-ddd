2bfdbac9fc7077545ad29256aef68f3fe8117567
Kirenai <pool_dota2@hotmail.com>
2024-03-03 16:19:43 -0500

    Add eureka client to consumption module - Upgrade the creation of WebClient Bean for each service - Add logs in UserClientAdapter and NourishmentClientAdapter Class

--- consumption/src/main/java/me/kire/re/consumption/infrastructure/adapter/client/UserClientAdapter.java
+++ consumption/src/main/java/me/kire/re/consumption/infrastructure/adapter/client/UserClientAdapter.java
@@ -1,24 +1,30 @@
 package me.kire.re.consumption.infrastructure.adapter.client;
 
-import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import me.kire.re.consumption.domain.model.dto.GetUserResponse;
 import me.kire.re.consumption.domain.port.out.client.UserClientPort;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @Component
-@RequiredArgsConstructor
 public class UserClientAdapter implements UserClientPort {
 
     private final WebClient webClient;
 
+    public UserClientAdapter(@Qualifier("webClientUser") WebClient webClient) {
+        this.webClient = webClient;
+    }
+
     @Override
     public Mono<GetUserResponse> getUserByUserId(Long userId) {
+        log.info("Invoking UserClientAdapter.getUserByUserId method");
         return this.webClient
                 .get()
-                .uri("http://localhost:8081/api/v0/users/{userId}", userId)
+                .uri("/api/v0/users/{userId}", userId)
                 .accept(MediaType.APPLICATION_JSON)
                 .exchangeToMono(clientResponse -> clientResponse.bodyToMono(GetUserResponse.class));
     }
--- consumption/src/main/java/me/kire/re/consumption/infrastructure/config/WebClientConfig.java
+++ consumption/src/main/java/me/kire/re/consumption/infrastructure/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package me.kire.re.consumption.infrastructure.config;
 
+import org.springframework.cloud.client.loadbalancer.reactive.ReactorLoadBalancerExchangeFilterFunction;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -8,8 +9,15 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfig {
 
     @Bean
-    public WebClient webClient(WebClient.Builder builder) {
-        return builder.build();
+    public WebClient webClientNourishment(WebClient.Builder builder,
+                                          ReactorLoadBalancerExchangeFilterFunction loadBalancerExchangeFilterFunction) {
+        return builder.baseUrl("http://NOURISHMENT").filter(loadBalancerExchangeFilterFunction).build();
+    }
+
+    @Bean
+    public WebClient webClientUser(WebClient.Builder builder,
+                                   ReactorLoadBalancerExchangeFilterFunction loadBalancerExchangeFilterFunction) {
+        return builder.baseUrl("http://USER").filter(loadBalancerExchangeFilterFunction).build();
     }
 
 }
